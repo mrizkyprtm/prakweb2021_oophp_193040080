276c5e409b8182456af6fe3f9d8bf6f542e934b2
m_rizkyprtm <m.rizkypratama319@gmail.com>
2021-09-17 19:54:52 +0700

    membuat constructor method di dalam class

--- constructor.php
+++ constructor.php
@@ -9,6 +9,14 @@ class Produk
     $penerbit = "penerbit",
     $harga = 0;
 
+  public function __construct($judul, $penulis, $penerbit, $harga)
+  {
+    $this->judul = $judul;
+    $this->penulis = $penulis;
+    $this->penerbit = $penerbit;
+    $this->harga = $harga;
+  }
+
   public function getLabel()
   {
     return "$this->penulis, $this->penerbit";
