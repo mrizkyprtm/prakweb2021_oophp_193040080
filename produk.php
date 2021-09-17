3e2bfa4131dee4352cce3dbaef2dc247251cfc56
m_rizkyprtm <m.rizkypratama319@gmail.com>
2021-09-17 19:43:38 +0700

    membuat method didalam class

--- produk.php
+++ produk.php
@@ -8,6 +8,11 @@ class Produk
     $penulis = "penulis",
     $penerbit = "penerbit",
     $harga = 0;
+
+  public function getLabel()
+  {
+    return "$this->penulis, $this->penerbit";
+  }
 }
 
 // $produk1 = new Produk();
@@ -19,11 +24,18 @@ class Produk
 // $produk2->tambahProperty = "hahaha";
 // var_dump($produk2);
 
-
 $produk3 = new Produk();
 $produk3->judul = "Naruto";
 $produk3->penulis = "Masashi Kishimoto";
 $produk3->penerbit = "Shonen Jump";
 $produk3->harga = 30000;
 
-echo "Komik : $produk3->penulis, $produk3->penerbit";
+$produk4 = new Produk();
+$produk4->judul = "Uncharted";
+$produk4->penulis = "Neil Druckmann";
+$produk4->penerbit = "Sony Computer";
+$produk4->harga = 250000;
+
+echo "Komik : " . $produk3->getLabel();
+echo "<br>";
+echo "Game : " . $produk4->getLabel();
