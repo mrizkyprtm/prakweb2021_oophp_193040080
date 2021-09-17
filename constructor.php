8432e12acc4d36995f9672b2dcc8e3fc75c7e2c0
m_rizkyprtm <m.rizkypratama319@gmail.com>
2021-09-17 20:04:08 +0700

    mengisi nilai property melalui parameter

--- constructor.php
+++ constructor.php
@@ -23,17 +23,9 @@ class Produk
   }
 }
 
-$produk3 = new Produk();
-$produk3->judul = "Naruto";
-$produk3->penulis = "Masashi Kishimoto";
-$produk3->penerbit = "Shonen Jump";
-$produk3->harga = 30000;
+$produk1 = new Produk("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000);
 
-$produk4 = new Produk();
-$produk4->judul = "Uncharted";
-$produk4->penulis = "Neil Druckmann";
-$produk4->penerbit = "Sony Computer";
-$produk4->harga = 250000;
+$produk2 = new Produk("Uncharted", "Neil Druckmann", "Sony Computer", 250000);
 
 echo "Komik : " . $produk3->getLabel();
 echo "<br>";
