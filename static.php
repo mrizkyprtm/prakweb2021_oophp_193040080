14366df994dc120739bac9fe3dca2136ca804f58
m_rizkyprtm <m.rizkypratama319@gmail.com>
2021-09-24 19:34:14 +0700

    (Pertemuan 3) membuat class baru dgn property tanpa keyword static

--- static.php
+++ static.php
@@ -1,17 +1,39 @@
 <?php
 
-class ContohStatic
-{
-  public static $angka = 1;
+// class ContohStatic
+// {
+//   public static $angka = 1;
 
-  public static function halo()
+//   public static function halo()
+//   {
+//     return "halo " . self::$angka++ . " kali.";
+//   }
+// }
+
+// echo ContohStatic::$angka;
+// echo "<br>";
+// echo ContohStatic::halo();
+// echo "<hr>";
+// echo ContohStatic::halo();
+
+class Contoh
+{
+  public $angka = 1;
+
+  public function halo()
   {
-    return "halo " . self::$angka++ . " kali.";
+    return "halo " . $this->angka++ . " kali.<br>";
   }
 }
 
-echo ContohStatic::$angka;
-echo "<br>";
-echo ContohStatic::halo();
+$obj = new Contoh;
+echo $obj->halo();
+echo $obj->halo();
+echo $obj->halo();
+
 echo "<hr>";
-echo ContohStatic::halo();
+
+$obj2 = new Contoh;
+echo $obj2->halo();
+echo $obj2->halo();
+echo $obj2->halo();
